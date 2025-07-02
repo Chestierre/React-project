2249839e66bd074ae176d56e62033a0b645f8387
Chestierre <chesterdoliente@gmail.com>
2025-07-02 23:46:44 +0800

    for today

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import ReactLogo from './assets/react.svg'
+
+function MyComponent() {
+  return (
+    <div>
+      <header>
+        <img src={ReactLogo} alt="React logo" /> 
+      </header>
+      {/*<h1>Create custom "Page" Component</h1>*/}
+      <main>
+        <h1>Reasons I'm excited to Learn React</h1>
+        <ol>
+          <li>Building</li>
+          <li>Serving</li>
+          <li>Learning</li>
+        </ol>
+      </main>
+      <footer>
+        © 2025 Doliente developmment. All right reserved
+      </footer>
+    </div>
+  )
+}
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'))
+root.render(<MyComponent />)
+
+
--- src/mainjsxprojects/componentChallenge.jsx
+++ src/mainjsxprojects/componentChallenge.jsx
@@ -1,17 +1,30 @@
 import { createRoot } from 'react-dom/client'
+import ReactLogo from './assets/react.svg'
+
+function MyComponent() {
+  return (
+    <div>
+      <header>
+        <img src={ReactLogo} alt="React logo" width ="40px" /> 
+      </header>
+      {/*<h1>Create custom "Page" Component</h1>*/}
+      <main>
+        <h1>Reasons I'm excited to Learn React</h1>
+        <ol>
+          <li>Building</li>
+          <li>Serving</li>
+          <li>Learning</li>
+        </ol>
+      </main>
+      <footer>
+        <small>© 2025 Doliente developmment. All right reserved</small>
+      </footer>
+    </div>
+  )
+}
+
 
 const root = createRoot(document.getElementById('root'))
 root.render(<MyComponent />)
 
-function MyComponent() {
-  return (
-    <main>
-      <h1>Create custom "Page" Component</h1>
-      <ol>
-        <li>Learning</li>
-        <li>Building</li>
-        <li>Serving</li>
-      </ol>
-    </main>
-  )
-}
+
